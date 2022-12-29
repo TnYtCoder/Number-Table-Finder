fd808a8822d1d8b134cf6aeccb454e13dd54ec92
TnYtCoder <115485810+TnYtCoder@users.noreply.github.com>
2022-12-29 21:28:23 +0530

    Create Table.py

---
+++ Table.py
@@ -0,0 +1,14 @@
+print("")
+print("𝚃𝚗𝚈𝚝𝙲𝚘𝚍𝚎𝚛")
+print("")
+print("")
+number = int(input("Select The No. To Find It's Table :- "))
+
+for count in range(1, 11):
+    product = number * count
+    print(number, "x", count, "=", product)
+    
+print("")
+print("")
+print("Created By TnYtCoder")
+print("")
